832014294b2b4881b31ed6b9ecb79da8d55871b9
izonejangwonyoung <2000shim@naver.com>
2021-05-07 17:17:23 +0900

    remove 구현

--- BinTree.c
+++ BinTree.c
@@ -14,7 +14,7 @@ static void setBinNode(BinNode *n, const Member *x, const BinNode *left, const B
     n->right = right;
 }
 
-BinNode *search(BinNode *p, const Member *x) {
+BinNode *Search(BinNode *p, const Member *x) {
     int cond;
     if (p == NULL) {
         return NULL;
@@ -25,7 +25,8 @@ BinNode *search(BinNode *p, const Member *x) {
     else if (cond < 0) {
         Search(p->left,x);
     }
-    else{
+    else
+    {
         Search(p->right,x);
     }
 
@@ -44,7 +45,49 @@ BinNode *Add(BinNode *p, const Member *x) {
     else if (cond < 0) {
         p->left =Add(p-> left,x);
     }
-    else{
+    else
+    {
         p->right= Add(p->right,x);
     }
+}
+
+/*--- 노드 삭제 ---*/
+int Remove(BinNode **root, const Member *x)
+{
+    BinNode *next, *temp;
+    BinNode **left;
+    BinNode **p = root;
+
+    while (1) {
+        int cond;
+        if (*p == NULL) {
+            printf("【오류】 %d는 등록되어 있지 않습니다.\n", x->no);
+            return -1;				/* 이 키는 없습니다. */
+        }
+        else if ((cond = MemberNoCmp(x, &(*p)->data)) == 0)
+            break;					/* 검색 성공 */
+        else if (cond < 0)
+            p = &((*p)->left);		/* 왼쪽 서브 트리에서 검색 */
+        else
+            p = &((*p)->right);		/* 오른쪽 서브 트리에서 검색 */
+    }
+
+    if ((*p)->left == NULL)
+        next = (*p)->right;
+    else {
+        left = &((*p)->left);
+        while ((*left)->right != NULL)
+            left = &(*left)->right;
+        next = *left;
+        *left = (*left)->left;
+        next->left = (*p)->left;
+        next->right = (*p)->right;
+    }
+
+    temp = *p;
+    *p = next;
+
+    free(temp);
+
+    return 0;
 }
